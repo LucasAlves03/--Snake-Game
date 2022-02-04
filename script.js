366b4f595790a20ee3ed2c5cc5b067b72ae596cb
LucasAlves03 <lucasalvesb.p018@hotmail.com>
2022-02-04 13:25:22 -0300

    Condições pra cobrinha comer

--- script.js
+++ script.js
@@ -52,8 +52,17 @@ iniciarJogo = () => {
     if(direction == 'left') snakeX -= box;
     if(direction == 'up') snakeY -= box;
     if(direction == 'down') snakeY += box;
+    
+    /*condições pra cobrinha comer */
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+    
+
     let newHead = {
         x: snakeX,
         y: snakeY
